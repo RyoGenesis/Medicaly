dae85c708d2c4cc82d44d308d0827fa17b8c3f3e
putuduta <putu.duta@gmail.com>
2021-05-07 21:47:05 +0700

    add datatables

--- Medicaly/Scripts/Product.js
+++ Medicaly/Scripts/Product.js
@@ -1,22 +1,26 @@
-﻿function filterTable(event) {
-    var filter = event.target.value.toUpperCase();
-    var rows = document.querySelector("#ProductList tbody").rows;
+﻿//function filterTable(event) {
+//    var filter = event.target.value.toUpperCase();
+//    var rows = document.querySelector("#ProductList tbody").rows;
 
-    for (var i = 0; i < rows.length; i++) {
-        var firstCol = rows[i].cells[1].textContent.toUpperCase();
-        var secondCol = rows[i].cells[2].textContent.toUpperCase();
-        var thirdCol = rows[i].cells[3].textContent.toUpperCase();
-        var fourthCol = rows[i].cells[4].textContent.toUpperCase();
-        var fifthCol = rows[i].cells[5].textContent.toUpperCase();
-        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1 || fourthCol.indexOf(filter) > -1 || fifthCol.indexOf(filter) > -1) {
-            rows[i].style.display = "";
-        } else {
-            rows[i].style.display = "none";
-        }
-    }
-}
+//    for (var i = 0; i < rows.length; i++) {
+//        var firstCol = rows[i].cells[1].textContent.toUpperCase();
+//        var secondCol = rows[i].cells[2].textContent.toUpperCase();
+//        var thirdCol = rows[i].cells[3].textContent.toUpperCase();
+//        var fourthCol = rows[i].cells[4].textContent.toUpperCase();
+//        var fifthCol = rows[i].cells[5].textContent.toUpperCase();
+//        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1 || fourthCol.indexOf(filter) > -1 || fifthCol.indexOf(filter) > -1) {
+//            rows[i].style.display = "";
+//        } else {
+//            rows[i].style.display = "none";
+//        }
+//    }
+//}
 
-document.querySelector('#mySearch').addEventListener('keyup', filterTable, false);
+//document.querySelector('#mySearch').addEventListener('keyup', filterTable, false);
+
+$(document).ready(function () {
+    $('#products').DataTable();
+});
 
 function AddProductPost(formData) {
     var ajaxConfig = {
@@ -65,6 +69,7 @@ function deleteProduct(val) {
             if (result.success) {
                 alert("Success Delete Product");
                 $('#tableList').load('/Pharmacy/Products/Manages #tableList');
+                $('#products').DataTable();
             }
         },
         error: function (err) {
